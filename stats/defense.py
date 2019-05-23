a080add803aa9b3cb3f12a14791624d2cd535f4e
Nick Johnson <36388801+nick-johnson333@users.noreply.github.com>
2019-05-22 23:35:34 -0500

    Update defense.py

--- stats/defense.py
+++ stats/defense.py
@@ -7,21 +7,17 @@ plays = games.query("type == 'play' & event != 'NP'")
 plays.columns = ['type','inning','team','player','count','pitches','event','game_id','year']
 
 pa = plays.loc[plays['player'].shift() != plays['player'], ['year','game_id','inning','team','player']]
-pa = pa.groupby(['year','game_id','team']).size().reset_index(name = 'PA')
+pa = pa.groupby(['year','game_id','team']).size().reset_index(name='PA')
 
 events = events.set_index(['year','game_id','team','event_type'])
 events = events.unstack().fillna(0).reset_index()
 
 events.columns = events.columns.droplevel()
 events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE','SO']
-events = events.rename_axis(None, axis = 'columns')
+events = events.rename_axis(None, axis='columns')
 
 events_plus_pa = pd.merge(events, pa, how='outer',left_on = ['year','game_id','team'],right_on = ['year','game_id','team'])
-
-
-
-
-defense = pd.merge(info, events_plus_pa)
+defense = pd.merge(events_plus_pa, info)
 
 defense.loc[:,'DER'] = 1 - ((defense['H'] + defense['ROE'])/(defense['PA'] - defense['BB'] - defense['SO'] - defense['HBP'] - defense['HR']))
 defense.loc[:,'year'] = pd.to_numeric(defense['year'])
